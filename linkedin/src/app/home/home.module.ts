f16cb9d4aeaed17116cd82c3b0d65787df29d72d
Dios Vo <vtmn1212@gmail.com>
2022-01-19 15:05:54 +0700

    cleanup: remove unnecessary code

--- linkedin/src/app/home/home.module.ts
+++ linkedin/src/app/home/home.module.ts
@@ -2,16 +2,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './components/header/header.component';
+import { PopoverComponent } from './components/header/popover/popover.component';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home.page.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [
+    HeaderComponent,
+    PopoverComponent,
+    HomePageComponent,
+  ],
   imports: [
     CommonModule,
+    HomePageRoutingModule,
+
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
   ],
 })
 export class HomePageModule { }
--- linkedin/src/app/home/home.page.component.ts
+++ linkedin/src/app/home/home.page.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.component.html',
-  styleUrls: ['home.page.component.scss'],
+  templateUrl: './home.page.component.html'
 })
-export class HomePageComponent {
-
-  constructor() { }
-
-}
+export class HomePageComponent { }
