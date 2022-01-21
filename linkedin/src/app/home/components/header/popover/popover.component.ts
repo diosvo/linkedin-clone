7811b91d42026c23b1b760fbb1e7184618765f1b
Dios Vo <vtmn1212@gmail.com>
2022-01-21 22:42:53 +0700

    update (linkedin/ popover): create const data for menu options

--- linkedin/src/app/home/components/header/popover/popover.component.ts
+++ linkedin/src/app/home/components/header/popover/popover.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-popover',
   templateUrl: './popover.component.html',
   styleUrls: ['./popover.component.scss']
 })
-export class PopoverComponent implements OnInit {
+export class PopoverComponent {
+
+  actions: { [key: string]: Array<string> } = {
+    account: ['setting & privacy', 'help', 'language'],
+    manage: ['posts & activities', 'job posting account']
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  onAction(action: string): void {
+    switch (action) {
+      case 'setting & privacy': {
+        this.signOut()
+      }
+    }
   }
 
+  signOut(): void {
+    console.log('aaa');
+  }
 }
