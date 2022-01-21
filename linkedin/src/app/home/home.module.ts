085b1e933824d7d03b322a008b4cdfdfa19ca9c2
Dios Vo <vtmn1212@gmail.com>
2022-01-21 23:22:03 +0700

    refactor (linkedin): move header component to global to be able to see it on every screen.

--- linkedin/src/app/home/home.module.ts
+++ linkedin/src/app/home/home.module.ts
@@ -1,24 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { HeaderComponent } from './components/header/header.component';
-import { PopoverComponent } from './components/header/popover/popover.component';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home.page.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    PopoverComponent,
-    HomePageComponent,
-  ],
+  declarations: [HomePageComponent],
   imports: [
     CommonModule,
     HomePageRoutingModule,
-
-    FormsModule,
-    IonicModule,
   ],
 })
 export class HomePageModule { }
---
+++ linkedin/src/app/utils/components/header/header.component.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header.component';
+import { PopoverComponent } from './popover/popover.component';
+
+@NgModule({
+  declarations: [HeaderComponent, PopoverComponent],
+  imports: [
+    CommonModule,
+
+    IonicModule,
+  ],
+  exports: [HeaderComponent],
+})
+export class HeaderModule { }
