b058b237fdc4471b7014ae21cb21416083751ad3
Dios Vo <vtmn1212@gmail.com>
2022-01-25 21:26:05 +0700

    feat (linkedin): advertising UI

--- linkedin/src/app/home/components/advertising/advertising.component.ts
+++ linkedin/src/app/home/components/advertising/advertising.component.ts
@@ -1,15 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-advertising',
-  templateUrl: './advertising.component.html',
-  styleUrls: ['./advertising.component.scss']
+  templateUrl: './advertising.component.html'
 })
-export class AdvertisingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class AdvertisingComponent { }
