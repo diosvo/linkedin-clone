5c3d54aaefff0da7dac8c81bd470bc27ddc30531
Dios Vo <vtmn1212@gmail.com>
2022-01-25 22:16:30 +0700

    feat (linkedin): start-post UI

--- linkedin/src/app/home/components/start-post/start-post.component.ts
+++ linkedin/src/app/home/components/start-post/start-post.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-start-post',
   templateUrl: './start-post.component.html',
   styleUrls: ['./start-post.component.scss']
 })
-export class StartPostComponent implements OnInit {
+export class StartPostComponent {
+
+  actions: Array<{ icon: string, fill: string, action: string }> = [
+    {
+      icon: 'image-outline',
+      fill: 'secondary',
+      action: 'photo'
+    },
+    {
+      icon: 'videocam-outline',
+      fill: 'success',
+      action: 'video'
+    },
+    {
+      icon: 'calendar-outline',
+      fill: 'warning',
+      action: 'event'
+    },
+    {
+      icon: 'newspaper-outline',
+      fill: 'danger',
+      action: 'write article'
+    }
+  ]
 
   constructor() { }
 
-  ngOnInit(): void {
+  onAction(action: string): void {
+    switch (action) {
+      case 'photo': {
+        console.log('aaaa');
+      }
+    }
   }
-
 }
